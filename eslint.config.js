19366092e2bae2702795c8de6016ba5aee3ab6e2
yuri <yuri.barros.luz.familia@gmail.com>
2024-12-13 13:13:59 -0300

    eslintfaill

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,22 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import { defineConfig } from "eslint-define-config";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+export default defineConfig({
+  env: {
+    browser: true,
+    es2021: true,
   },
-)
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "@rocketseat/eslint-config/react",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    // Adicione regras personalizadas aqui, se necessário
+  },
+});
